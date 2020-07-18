5cd1288faae09f2d8ef896299c2f0ffd746b1213
Adam Golec <algolec1990@yahoo.com>
2020-07-18 15:09:03 -0400

    initial commit

---
+++ src/OverLoading.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class OverLoading {
+
+    public static void main(String[] args)
+    {
+        int iNumber;
+        double dNumber;
+
+        //Create a scanner object to use for keyboard input.
+        Scanner keyboard = new Scanner(System.in);
+
+        //Get an integer and display it's square.
+        System.out.println("Enter an integer: ");
+        iNumber = keyboard.nextInt();
+        System.out.println("The square is "
+                + MyMath.square(iNumber) + " for int.");
+
+        System.out.println("Enter an double: ");
+        dNumber = keyboard.nextDouble();
+        System.out.println("The double is " + MyMath.square(dNumber) + " when squared.");
+
+    }
+}
---
+++ src/t/JavaClassHere.java
@@ -0,0 +1,8 @@
+package t;
+
+public class JavaClassHere {
+    public static void main(String[] args)
+    {
+        System.out.println("Bullshit");
+    }
+}
