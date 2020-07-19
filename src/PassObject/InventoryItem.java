18203e7680ba9916e0066d73c74553123bc74923
Adam Golec <algolec1990@yahoo.com>
2020-07-19 16:03:44 -0400

    added more exmples of concepts in chapter 6. Passing objects as arguments, equals methods, etc

---
+++ src/PassObject/InventoryItem.java
@@ -0,0 +1,55 @@
+package PassObject;
+
+import java.util.Scanner;
+
+public class InventoryItem {
+
+    //Item description and units on-hand.
+    private String description;
+    private int units;
+
+    //No-arg consructor.
+    public InventoryItem(){
+        description = "";
+        units = 0;
+    }
+    //Constructor meant to take in a string for the name, and string for the units.
+    public InventoryItem(String name, int units)
+    {
+        this.description = name;
+        this.units = units;
+    }
+    //Constructor for only a description being passed in.
+    public InventoryItem(String name){
+        this.description = name;
+        units = 0;
+    }
+    /**
+     * The setUnits method assigns its argument
+     * to the units field.
+     */
+    public void setUnits(int u)
+    {
+        units = u;
+    }
+    /**
+     * The getDescription method returns the
+     * value in a description field.
+     */
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    /**
+     * The getUnits method returns the value in units field.
+     */
+    public int getUnits()
+    {
+        return units;
+    }
+    //Static method for calling getData() in order to set up an instance of
+    //InventoryItem by calling the String/Int constructor on line 17.
+
+}
