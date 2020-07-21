53ab5f41e15f9586e3c32edc1af2883e0e07a7fa
Adam Golec <algolec1990@yahoo.com>
2020-07-20 21:21:34 -0400

    adding new things up to the enumeration section of chapter 6

---
+++ src/RetailItem/InnerDemo.java
@@ -0,0 +1,12 @@
+package RetailItem;
+
+public class InnerDemo
+{
+    public static void main(String[] args)
+    {
+        //Create a RetailItem object.
+        RetailItem item = new RetailItem("傘", 0003014, 300,400);
+
+        System.out.println(item);
+    }
+}
