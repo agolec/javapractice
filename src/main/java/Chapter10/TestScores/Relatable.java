dd9725d609c719d6683e3b81e48a9aab262887b7
Adam <algolec1990@yahoo.com>
2023-06-15 23:19:32 -0400

    make interface that TestScores class will use to implement methods that will perform different operations against the array of test scores.

--- src/main/java/Chapter10/TestScores/Relatable.java
+++ src/main/java/Chapter10/TestScores/Relatable.java
@@ -1,2 +1,9 @@
-package Chapter10.TestScores;public interface Relatable {
+package Chapter10.TestScores;
+
+public interface Relatable {
+    public int getAverage();
+    public int getMax();
+    public int getMin();
+    public int getRange();
+    public int getTotal();
 }
